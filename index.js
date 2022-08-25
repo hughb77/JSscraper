1452ae2789773bd5c784925e34262722ca9e52d7
hugh77 <thebasslineartist@gmail.com>
2022-08-25 02:37:01 -0400

    primerCommit

---
+++ index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const PORT = 8000;
+
+const app = express();
+const url = 'https://doblaje.fandom.com/es/wiki/Los_Caballeros_del_Zodiaco';
+app.listen(PORT, () => console.log(`SERVIDOR ${PORT}`));
+const characters = [];
+var fs = require('fs');
+const filename = 'ONE.txt';
+
+axios(url).then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('td', html).each(function () {
+         const name = $(this).find('a').text();
+         characters.push(name);
+      });
+      fs.writeFile(filename, characters.toString(), function (err) {
+         if (err) {
+            console.log(err);
+         }
+         console.log(characters);
+      });
+   })
+   .catch((err) => console.error(err));
